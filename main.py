5684c069e526480428a6fefde101e97b0da3d834
TantoFaz <thiago.playgames15@gmail.com>
2023-01-01 12:12:29 -0300

    :D

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-# Install
-**Pypi**
-```
-pip install abreai
-```
-
-**GitHub**
-```
-pip install git+https://github.com/1Marcuth/encurtanet-py.git
-```
-
-# Simple use example
-```py
 from abreai import AbreAi
 
 shortener = AbreAi()
@@ -22,4 +9,3 @@ url_info = shortener.shorten(
 shortened_url = url_info.get_shortened_url()
 
 print(url_info.get_shortened_url())
-```
